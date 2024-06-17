504487f71fd20822fd5ff0ace3b80e2a51591d35
Naymul-NN <naymulislam108@gmail.com>
2024-06-17 08:38:57 +0600

    auth middleware

--- src/app/modules/academicFacalty/academicFacalty.route.ts
+++ src/app/modules/academicFacalty/academicFacalty.route.ts
@@ -4,11 +4,12 @@ import express from "express";
 import { AcademicFacaltyController } from "./academicFacalty.controller";
 import validationRequest from "../../middleware/validateRequest";
 import { academicFacaltyVlidation } from "./academicFacalty.validation";
+import auth from "../../middleware/auth";
 
 const router = express.Router()
 
 router.post('/create-academic-facalty', validationRequest(academicFacaltyVlidation.academicFacaltyVlidationSchema), AcademicFacaltyController.createAcademicFacalty)
-router.get('/', AcademicFacaltyController.getAllAcademicFacalty);
+router.get('/',auth(), AcademicFacaltyController.getAllAcademicFacalty);
 router.get('/:FacaltyId', AcademicFacaltyController.getSingleAcademicFacalty);
 
 router.patch(
