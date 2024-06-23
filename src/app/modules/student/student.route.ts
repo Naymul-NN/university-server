9ff43bbc04ff0ec7864ea9bcf379ef5d864b43d8
Naymul-NN <naymulislam108@gmail.com>
2024-06-23 20:20:44 +0600

    student: auth(user-role.admin )

--- src/app/modules/student/student.route.ts
+++ src/app/modules/student/student.route.ts
@@ -4,11 +4,15 @@ import express from "express";
 import { studentController } from "./student.controller";
 import validationRequest from "../../middleware/validateRequest";
 import { studentvalidation } from "./student.validation";
+import auth from "../../middleware/auth";
+import { USER_ROLE } from "../user/user.constant";
 
 const router = express.Router()
 
 // router.post('/create-student',studentController.createStudent)
-router.get('/', studentController.getAllStudents);
+router.get('/',
+    auth(USER_ROLE.admin),
+    studentController.getAllStudents);
 
 router.get('/:studentId', studentController.getSingleStudent);
 router.delete('/:studentId', studentController.deleteSingleStudent);
