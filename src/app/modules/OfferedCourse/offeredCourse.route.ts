6df0c44c0cb2795e78e2a90fb4c9c4e37ae68e30
Naymul-NN <naymulislam108@gmail.com>
2024-06-25 22:26:06 +0600

    offered course secured

--- src/app/modules/OfferedCourse/offeredCourse.route.ts
+++ src/app/modules/OfferedCourse/offeredCourse.route.ts
@@ -2,6 +2,8 @@ import express from 'express';
 import { OfferedCourseControllers } from './offeredCourse.controller';
 import validationRequest from '../../middleware/validateRequest';
 import { OfferedCourseValidations } from './offeredCourse.validation';
+import auth from '../../middleware/auth';
+import { USER_ROLE } from '../user/user.constant';
 
 
 const router = express.Router();
@@ -12,12 +14,14 @@ router.get('/:id', OfferedCourseControllers.getSingleOfferedCourses);
 
 router.post(
   '/create-offered-course',
+  auth(USER_ROLE.admin),
   validationRequest(OfferedCourseValidations.createOfferedCourseValidationSchema),
   OfferedCourseControllers.createOfferedCourse,
 );
 
 router.patch(
   '/:id',
+  auth(USER_ROLE.admin),
   validationRequest(OfferedCourseValidations.updateOfferedCourseValidationSchema),
   OfferedCourseControllers.updateOfferedCourse,
 );
